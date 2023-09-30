f5541e4e4a0cb6f03c14da6adc573a3bd83a9bd0
Taliah-B <85928465+Taliah-B@users.noreply.github.com>
2023-09-30 20:29:43 +1000

    Set up game
    Can play 3 rounds of rock paper scissors through command line

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,83 @@
 
+const options = ["rock", "paper", "scissors"];
+
+//generates a random option for the computer player
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random() * options.length)]
+    return choice;
+}
+
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return "Draw";
+    }
+    else if(
+        (playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "rock")
+    ){
+        return "Player";
+    }
+    else{
+        return "Computer"
+    }
+}
+
+function playRound(playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if (result == "Draw" ){
+        return `Draw! You both picked ${playerSelection}`
+    }
+    else if(result == "Player"){
+        return `You win! ${playerSelection} beats ${computerSelection}`
+    }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
+function getPlayerChoice(){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt("Rock Paper Scissors");
+        if(choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log("Welcome!")
+    for (let i = 0; i < 3; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+        if(checkWinner(playerSelection, computerSelection) == "Player"){
+        scorePlayer++;
+    }
+    if(checkWinner(playerSelection, computerSelection) == "Computer"){
+        scoreComputer++;
+    }
+    }
+    console.log("Game Over");
+    if(scorePlayer > scoreComputer){
+        console.log("You Won!")
+    }
+    else if(scorePlayer < scoreComputer){
+        console.log("You Lost!")
+    }
+    else{
+        console.log("Draw!")
+    }
+}
+game()
+
+
+console.log(playRound(playerSelection, computerSelection));
+
+getComputerChoice();
